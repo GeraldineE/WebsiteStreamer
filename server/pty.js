c0619576502c4f9c330b0665453ff83c85b679fe
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-30 10:46:49 -0300

    hashcode

--- server/pty.js
+++ server/pty.js
@@ -32,7 +32,7 @@ Container.prototype.run = function(on_data) {
     const bash = pty.spawn("/bin/bash");
 
     bash.write(command)
-    bash.write("cd /home/" + hashcode + "\n")  
+    bash.write("cd /home/" + this.hashcode + "\n")  
     
     bash.on('data', (data) => {
         on_data(data);
