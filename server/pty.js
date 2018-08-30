223a76c3c440ae0f2858be9f690f0dfca01c0adb
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-29 23:12:37 -0300

    submodule

--- server/pty.js
+++ server/pty.js
@@ -7,12 +7,13 @@ var Container = function (hashcode, language) {
 };
 
 Container.prototype.run = function(on_data) {
-    command = "docker run --rm --name " + hashcode +  " -it " + language +  " /bin/bash \n";
+    command = "docker run --rm --name " + hashcode + " -v ./snippets/" +  hashcode + ":/" + hashcode + " -it " + language +  " /bin/bash \n";
     console.log(command);
     const bash = pty.spawn("/bin/bash");
 
     bash.write(command)
-    bash.write("clear")
+    bash.write("cd " + hashcode + "\n")
+    bash.write("clear \n")
 
     bash.on('data', (data) => {
         on_data(data);
