388253c9e471a25b86dbe33b9614c900be882739
joelibaceta <joel.ibaceta@mercadolibre.com>
2018-08-30 10:05:40 -0300

    docker attach

--- server/pty.js
+++ server/pty.js
@@ -8,11 +8,9 @@ var Container = function (hashcode, language) {
 
 Container.prototype.isRunning = function() {
     var bash_if = "if [ $(docker inspect -f '{{.State.Running}}' '" + this.hashcode + "-" + this.language +  "') = 'true' ]; then echo yup; else echo nope; fi";
-    const child_process = require('child_process');
     var response = child_process.execSync(bash_if);
-
-    console.log("status: " + response.toString());
-    return (response.toString() != 'yup');
+    
+    return (response.toString() == 'yup');
 
 };
 
@@ -21,9 +19,9 @@ Container.prototype.run = function(on_data) {
     console.log(this.isRunning());
 
     if (this.isRunning()) {
-        command = "docker run --rm --name " + this.hashcode + "-" + this.language + " -v $(pwd)/snippets/" +  hashcode + ":/home/" + hashcode + " -it " + language +  " /bin/bash \n";
-    } else {
         command = "docker attach " + this.hashcode + "-" + this.language + " \n"
+    } else {
+        command = "docker run --rm --name " + this.hashcode + "-" + this.language + " -v $(pwd)/snippets/" +  hashcode + ":/home/" + hashcode + " -it " + language +  " /bin/bash \n";
     }
 
 
